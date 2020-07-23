9b2f0c8d50d1caafa500ea41140048ef9635b27f
diegojsanches <diegojsanches@hotmail.com>
2020-07-23 01:29:45 -0300

    Adição de Procfile

--- ormconfig.js
+++ ormconfig.js
@@ -2,10 +2,10 @@ module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
     "entities": [
-      "./dist/modules/**/infra/typeorm/entities/*.ts"
+      "./dist/modules/**/infra/typeorm/entities/*.js"
     ],
     "migrations": [
-      "./dist/shared/infra/typeorm/migrations/*.ts"
+      "./dist/shared/infra/typeorm/migrations/*.js"
     ],
     "cli": {
       "migrationsDir": "./src/shared/infra/typeorm/migrations",
