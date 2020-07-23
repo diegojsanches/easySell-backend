c79f0add0db3a809be7ccb3eee2a16f8b58bc7c1
diegojsanches <diegojsanches@hotmail.com>
2020-07-23 00:25:26 -0300

    Adição de Procfile

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "name": "default",
+    "url": process.env.DATABASE_URL,
+    "entities": [
+      "./src/modules/**/infra/typeorm/entities/*.ts"
+    ],
+    "migrations": [
+      "./src/shared/infra/typeorm/migrations/*.ts"
+    ],
+    "cli": {
+      "migrationsDir": "./src/shared/infra/typeorm/migrations"
+    }
+  };
