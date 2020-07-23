7e379bcbc281f54390540fae3e8de4d097a7d9db
diegojsanches <diegojsanches@hotmail.com>
2020-07-23 01:14:06 -0300

    Adição de Procfile

--- dist/src/config/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: process.env.APP_SECRET || 'Test-secret',
-        expiresIn: '12h',
-    },
-};
