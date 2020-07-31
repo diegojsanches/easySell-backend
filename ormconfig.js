a0940d298e23d08e876038a217709b4e4416b23e
diegojsanches <diegojsanches@hotmail.com>
2020-07-31 17:35:48 -0300

    Ajuste de pesquisa de vendas pordata

--- ormconfig.js
+++ ormconfig.js
@@ -2,12 +2,12 @@ module.exports = {
   "name": "default",
   "type": "postgres",
   "url": process.env.DATABASE_URL,
-  // "ssl": true,
-  // "extra": {
-  //   "ssl":  {
-  //     rejectUnauthorized: false,
-  //   },
-  // },
+  "ssl": true,
+  "extra": {
+    "ssl":  {
+      rejectUnauthorized: false,
+    },
+  },
   "entities": [
     process.env.DATABASE_ENTITIES
   ],
--- src/modules/sales/infra/http/controllers/SalesController.ts
+++ src/modules/sales/infra/http/controllers/SalesController.ts
@@ -4,13 +4,13 @@ import { container } from 'tsyringe';
 import CreateSaleService from '@modules/sales/services/CreateSaleService';
 import ListSalesService from '@modules/sales/services/ListSalesService';
 import ShowSaleService from '@modules/sales/services/ShowSaleService';
-import { addDays, endOfDay } from 'date-fns';
+import { addDays, endOfDay, startOfDay } from 'date-fns';
 
 class SalesController {
   public async index(req: Request, res: Response): Promise<Response> {
     const { fromDate, toDate } = req.query;
-    const parsedFromDate = endOfDay(new Date(fromDate as string));
-    const parsedToDate = endOfDay(addDays(new Date(toDate as string), 1));
+    const parsedFromDate = startOfDay(new Date(fromDate as string));
+    const parsedToDate = endOfDay(new Date(toDate as string));
     console.log(parsedFromDate, parsedToDate);
 
     const listSales = container.resolve(ListSalesService);
