5a9457ee67e454d89627610d374ba50c2cd5d3df
diegojsanches <diegojsanches@hotmail.com>
2020-07-30 22:27:50 -0300

    Ajustes no signIn

---
+++ Procfile
@@ -0,0 +1,1 @@
+release: yarn typeorm migration:run
--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "type": "postgres",
-    "url": process.env.DATABASE_URL,
-    "entities": [
-      "./dist/modules/**/infra/typeorm/entities/*.js"
-    ],
-    "migrations": [
-      "./dist/shared/infra/typeorm/migrations/*.js"
-    ],
-    "cli": {
-      "migrationsDir": "./src/shared/infra/typeorm/migrations",
-      "entitiesDir": "./src/modules/**/infra/typeorm/entities"
-    }
-  };
+  "name": "default",
+  "type": "postgres",
+  "url": process.env.DATABASE_URL,
+  "entities": [
+    "./src/modules/**/infra/typeorm/entities/*.ts"
+  ],
+  "migrations": [
+    "./src/shared/infra/typeorm/migrations/*.ts"
+  ],
+  "cli": {
+    "migrationsDir": "./src/shared/infra/typeorm/migrations"
+  }
+};
