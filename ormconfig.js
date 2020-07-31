bcffd8e1a99ccf3d5f64ce158df5938946756580
diegojsanches <diegojsanches@hotmail.com>
2020-07-30 22:39:21 -0300

    Ajustes no signIn

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,12 @@ module.exports = {
   "name": "default",
   "type": "postgres",
   "url": process.env.DATABASE_URL,
+  "ssl": true,
+  "extra": {
+    "ssl":  {
+      rejectUnauthorized: false,
+    },
+  },
   "entities": [
     "./src/modules/**/infra/typeorm/entities/*.ts"
   ],
